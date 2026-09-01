44f1848616bd21798fd48b895f6cc84c846403be
RubensPaesNeto <rubenspaesneto@gmail.com>
2026-09-01 20:35:29 -0300

    feat
    colocada as rotas de autenticação

--- routes/veiculoRoutes.js
+++ routes/veiculoRoutes.js
@@ -2,15 +2,17 @@ const router = require("express").Router();
 
 const VeiculoController = require("../controllers/VeiculoController");
 
-router.get("/todos", VeiculoController.buscar);
-router.get("/cadastro", VeiculoController.formCadastro);
-router.post("/cadastro", VeiculoController.cadastrar);
+const UsuarioController = require("../controllers/UsuarioController");
 
-router.get("/buscar/:id", VeiculoController.buscarPorId);
+router.get("/todos", UsuarioController.verificaAutenticacao, VeiculoController.buscar);
+router.get("/cadastro", UsuarioController.verificaAutenticacao, VeiculoController.formCadastro);
+router.post("/cadastro", UsuarioController.verificaAutenticacao, VeiculoController.cadastrar);
 
-router.get("/editar/:id", VeiculoController.formEditar);
-router.post("/editar/:id", VeiculoController.editar);
+router.get("/buscar/:id", UsuarioController.verificaAutenticacao, VeiculoController.buscarPorId);
 
-router.post("/deletar/:id", VeiculoController.deletar);
+router.get("/editar/:id", UsuarioController.verificaAutenticacao, VeiculoController.formEditar);
+router.post("/editar/:id", UsuarioController.verificaAutenticacao,  VeiculoController.editar);
+
+router.post("/deletar/:id", UsuarioController.verificaAutenticacao,  VeiculoController.deletar);
 
 module.exports = router;
